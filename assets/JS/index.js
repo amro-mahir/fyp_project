88af8fa4b041859872641816b39ec3a91de06053
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-07-01 01:13:31 +0800

    prediction model added for each crypto

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -33,4 +33,119 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=us
   .catch(error => {
     console.log('An error occurred while fetching the Tether price:', error);
   });
-  
+
+// prediction algorithm for bitcoin  
+// Function to fetch the previous price of Bitcoin for each day in the past week
+// Function to calculate the weekly average price of Bitcoin for the last 4 weeks
+async function calculateWeeklyAverageBitcoinPrice() {
+  const weeks = 4; // Number of weeks
+  const currentDate = new Date();
+  const weeklyAverages = [];
+
+  for (let i = 0; i < weeks; i++) {
+    const endDate = new Date(currentDate.getTime() - (i * 7 * 24 * 60 * 60 * 1000)); // Subtracting the required number of weeks in milliseconds
+    const startDate = new Date(endDate.getTime() - (6 * 24 * 60 * 60 * 1000)); // Subtracting 6 days in milliseconds
+
+    const priceDataResponse = await fetch(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=${startDate.getTime() / 1000}&to=${endDate.getTime() / 1000}`);
+    const priceData = await priceDataResponse.json();
+
+    const prices = priceData.prices;
+    if (prices.length > 0) {
+      const weeklySum = prices.reduce((sum, price) => sum + price[1], 0);
+      const weeklyAverage = weeklySum / prices.length;
+      weeklyAverages.push(weeklyAverage);
+    }
+  }
+
+  return weeklyAverages;
+}
+
+// Call the function to calculate the weekly average Bitcoin prices
+calculateWeeklyAverageBitcoinPrice()
+  .then(weeklyAverages => {
+    console.log('Weekly average Bitcoin prices for the last 4 weeks:');
+    weeklyAverages.forEach((average, index) => {
+      const weekNumber = index + 1;
+      console.log(`Week ${weekNumber}: ${average}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error calculating weekly average Bitcoin prices:', error);
+  });
+
+
+// prediction model for etheruem
+// Function to calculate the weekly average price of Ethereum for the last 4 weeks
+async function calculateWeeklyAverageEthereumPrice() {
+  const weeks = 4; // Number of weeks
+  const currentDate = new Date();
+  const weeklyAverages = [];
+
+  for (let i = 0; i < weeks; i++) {
+    const endDate = new Date(currentDate.getTime() - (i * 7 * 24 * 60 * 60 * 1000)); // Subtracting the required number of weeks in milliseconds
+    const startDate = new Date(endDate.getTime() - (6 * 24 * 60 * 60 * 1000)); // Subtracting 6 days in milliseconds
+
+    const priceDataResponse = await fetch(`https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=${startDate.getTime() / 1000}&to=${endDate.getTime() / 1000}`);
+    const priceData = await priceDataResponse.json();
+
+    const prices = priceData.prices;
+    if (prices.length > 0) {
+      const weeklySum = prices.reduce((sum, price) => sum + price[1], 0);
+      const weeklyAverage = weeklySum / prices.length;
+      weeklyAverages.push(weeklyAverage);
+    }
+  }
+
+  return weeklyAverages;
+}
+
+// Call the function to calculate the weekly average Ethereum prices
+calculateWeeklyAverageEthereumPrice()
+  .then(weeklyAverages => {
+    console.log('Weekly average Ethereum prices for the last 4 weeks:');
+    weeklyAverages.forEach((average, index) => {
+      const weekNumber = index + 1;
+      console.log(`Week ${weekNumber}: ${average}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error calculating weekly average Ethereum prices:', error);
+  });
+
+  // prediction model for tether
+  // Function to calculate the weekly average price of Tether for the last 4 weeks
+async function calculateWeeklyAverageTetherPrice() {
+  const weeks = 4; // Number of weeks
+  const currentDate = new Date();
+  const weeklyAverages = [];
+
+  for (let i = 0; i < weeks; i++) {
+    const endDate = new Date(currentDate.getTime() - (i * 7 * 24 * 60 * 60 * 1000)); // Subtracting the required number of weeks in milliseconds
+    const startDate = new Date(endDate.getTime() - (6 * 24 * 60 * 60 * 1000)); // Subtracting 6 days in milliseconds
+
+    const priceDataResponse = await fetch(`https://api.coingecko.com/api/v3/coins/tether/market_chart/range?vs_currency=usd&from=${startDate.getTime() / 1000}&to=${endDate.getTime() / 1000}`);
+    const priceData = await priceDataResponse.json();
+
+    const prices = priceData.prices;
+    if (prices.length > 0) {
+      const weeklySum = prices.reduce((sum, price) => sum + price[1], 0);
+      const weeklyAverage = weeklySum / prices.length;
+      weeklyAverages.push(weeklyAverage);
+    }
+  }
+
+  return weeklyAverages;
+}
+
+// Call the function to calculate the weekly average Tether prices
+calculateWeeklyAverageTetherPrice()
+  .then(weeklyAverages => {
+    console.log('Weekly average Tether prices for the last 4 weeks:');
+    weeklyAverages.forEach((average, index) => {
+      const weekNumber = index + 1;
+      console.log(`Week ${weekNumber}: ${average}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error calculating weekly average Tether prices:', error);
+  });
