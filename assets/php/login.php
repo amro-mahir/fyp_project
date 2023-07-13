81623ca08caabf8bd81a274c70bccf59e1dc9dbc
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-07-13 15:17:48 +0800

    login feature added

---
+++ assets/php/login.php
@@ -0,0 +1,33 @@
+<?php
+
+$email = $_POST['email'];
+$userPassword = $_POST['password'];
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'fyp';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo 'connection success';
+}
+
+$sql = "SELECT * FROM users WHERE email='$email' AND password='$userPassword'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Login successful
+    echo "Login successful!";
+} else {
+    // Login failed
+    echo "Invalid username or password";
+}
+
+// Close the connection
+$conn->close();
+
+?>
