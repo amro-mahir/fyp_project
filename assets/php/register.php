bc9bb46283a35604ae44b2cd8bf1b481385c2f27
Amro Mahir Suliman Fadlelbari <amro@auronex.com>
2023-07-14 15:07:55 +0800

    register error added

--- assets/php/register.php
+++ assets/php/register.php
@@ -19,15 +19,37 @@ if ($conn->connect_error) {
     echo 'connection success';
 }
 
-$sql = "INSERT INTO user (firstName, lastName, birthDate, email, password) VALUES ('$firstName', '$lastName', '$birthDate', '$userEmail', '$userPassword')";
+// $sql = "INSERT INTO user (firstName, lastName, birthDate, email, password) VALUES ('$firstName', '$lastName', '$birthDate', '$userEmail', '$userPassword')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Data inserted successfully!";
-    header("Location: http://localhost/fyp_project/homepage.html");
+$sql = "SELECT * FROM user WHERE email='$userEmail'";
+$result = $conn->query($sql);
+
+// Check if the query returned any rows
+if ($result->num_rows > 0) {
+    // Email already in use
+    echo "Email is already in use. Please choose a different email.";
+    header("Location: http://localhost/fyp_project/registerError.html");
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    header("Location: https://localhost/fyp_project/register.html");
+    // Insert the new user into the database
+    $insertQuery = "INSERT INTO user (firstName, lastName, birthDate, email, password) VALUES ('$firstName', '$lastName', '$birthDate', '$userEmail', '$userPassword')";
+    if ($conn->query($insertQuery) === TRUE) {
+        // Registration successful
+        echo "Registration successful!";
+        header("Location: http://localhost/fyp_project/homepage.html");
+    } else {
+        // Registration failed
+        // echo "Error registering user: " . $conn->error;
+        header("Location: http://localhost/fyp_project/registerError.html");
+    }
 }
+
+// if ($conn->query($sql) === TRUE) {
+//     echo "Data inserted successfully!";
+//     header("Location: http://localhost/fyp_project/homepage.html");
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+//     header("Location: https://localhost/fyp_project/register.html");
+// }
 
 $conn->close();
 ?>
